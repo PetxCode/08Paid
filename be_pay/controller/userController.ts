3d9a3ab03ac3200c30096e9a39042b3c446b5e31
PetxCode <brighterdayscodelab@gmail.com>
2024-02-08 01:41:10 +0100

    adding verify

--- be_pay/controller/userController.ts
+++ be_pay/controller/userController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import userModel from "../model/userModel";
 import { CronJob } from "cron";
 import moment from "moment";
+import axios from "axios";
 const https = require("https");
 
 export const createUser = async (req: Request, res: Response) => {
@@ -214,53 +215,44 @@ export const makePayment = async (req: Request, res: Response) => {
 
 export const verifyPayment = async (req: Request, res: Response) => {
   try {
-    const trxref = req.params;
-    console.log(trxref);
-
-    // const params = JSON.stringify({
-    //   email,
-    //   amount: (parseInt(amount) * 100).toString(),
-    //   callback_url: "http://localhost:5173/main",
-    //   metadata: {
-    //     cancel_action: "http://localhost:5173/action",
-    //   },
-    //   channels: ["card"],
-    // });
+    const { trxref } = req.params;
 
     const options = {
       hostname: "api.paystack.co",
       port: 443,
-      path: `/transaction/verify/:reference`,
+      path: `/transaction/verify/${trxref}`,
       method: "GET",
       headers: {
         Authorization:
           "Bearer sk_test_ec1b0ccabcb547fe0efbd991f3b64b485903c88e",
-        "Content-Type": "application/json",
       },
     };
 
-    const request = https
-      .request(options, (response: any) => {
+    https
+      .request(options, (res: any) => {
         let data = "";
 
-        response.on("data", (chunk: any) => {
+        res.on("data", (chunk: any) => {
           data += chunk;
         });
 
-        response.on("end", () => {
-          return res.status(201).json({
-            message: "welcome",
+        res.on("end", () => {
+          return res.status(420004).json({
+            message: "Error",
             data: JSON.parse(data),
-            status: 201,
+            status: 200,
           });
         });
       })
       .on("error", (error: any) => {
-        console.error(error);
+        return res.status(404).json({
+          message: "Error",
+          data: error.message,
+          status: 404,
+        });
       });
 
-    // request.write(params);
-    // request.end();
+    console.log("Eneded");
   } catch (error: any) {
     return res.status(404).json({
       message: "Error",
@@ -280,6 +272,74 @@ export const viewOneUser = async (req: Request, res: Response) => {
       data: user,
       status: 201,
     });
+  } catch (error: any) {
+    return res.status(404).json({
+      message: "Error",
+      data: error.message,
+      status: 404,
+    });
+  }
+};
+
+export const viewVerifyTransaction = async (req: Request, res: Response) => {
+  try {
+    const { ref } = req.params;
+    console.log(ref);
+
+    await axios
+      .get(`https://api.paystack.co/transaction/verify/${ref}`, {
+        headers: {
+          authorization:
+            "Bearer sk_test_ec1b0ccabcb547fe0efbd991f3b64b485903c88e",
+
+          "content-type": "application/json",
+          "cache-control": "no-cache",
+        },
+      })
+      .then((resp) => {
+        return res.status(201).json({
+          message: "welcome",
+          data: resp.data,
+          status: 201,
+        });
+      });
+  } catch (error: any) {
+    return res.status(404).json({
+      message: "Error",
+      data: error.message,
+      error: error,
+      status: 404,
+    });
+  }
+};
+
+export const makeTransaction = async (req: Request, res: Response) => {
+  try {
+    const { email, amount } = req.body;
+
+    const data = {
+      email,
+      amount,
+    };
+
+    await axios
+      .post(`https://api.paystack.co/transaction/initialize`, data, {
+        headers: {
+          authorization:
+            "Bearer sk_test_ec1b0ccabcb547fe0efbd991f3b64b485903c88e",
+
+          "content-type": "application/json",
+          "cache-control": "no-cache",
+        },
+      })
+      .then((resp) => {
+        console.log(resp.data);
+        return res.status(201).json({
+          message: "transaction initialize",
+          data: resp.data,
+          status: 201,
+        });
+      });
   } catch (error: any) {
     return res.status(404).json({
       message: "Error",
--- be_pay/router/userRouter.ts
+++ be_pay/router/userRouter.ts
@@ -4,9 +4,11 @@ import {
   createUser,
   loginUser,
   makePayment,
+  makeTransaction,
   verifyPayment,
   viewOneUser,
   viewUser,
+  viewVerifyTransaction,
 } from "../controller/userController";
 
 const router: Router = Router();
@@ -19,6 +21,9 @@ router.route("/view/:userID").get(viewOneUser);
 router.route("/make-payment").post(makePayment);
 router.route("/active/:userID").patch(activeUser);
 
-router.route("/verify/").get(verifyPayment);
+router.route("/verifyx/:trxref").get(verifyPayment);
+router.route("/verify/:ref").get(viewVerifyTransaction);
+
+router.route("/make").post(makeTransaction);
 
 export default router;
